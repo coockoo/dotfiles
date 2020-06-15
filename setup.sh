e0e161da7ce0ffe875ac4b3871eae0bdcd93d352
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-06-15 17:05:46 +0300

    Add vim colorscheme install and more logging to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,28 +3,81 @@
 # Set zsh as default shell
 EXPECTED_SHELL="/bin/zsh"
 
+echo "Setting default shell to zsh..."
 if ! [ "$SHELL" == "$EXPECTED_SHELL" ]; then
-  echo "Setting default shell to zsh..."
   chsh -s /bin/zsh
+  echo "zsh is now a default shell"
+else
+  echo "zsh is already a default shell"
 fi
 
-# Check if oh-my-zsh is installed
+echo "Installing oh-my-zsh..."
 if ! [ -d "$HOME/.oh-my-zsh" ]; then
-  echo "Installing oh-my-zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "Successfully installed oh-my-zsh"
+else
+  echo "oh-my-zsh is already installed"
 fi
 
-# Install diff so fancy
+echo "Installing diff-so-fancy..."
 if ! [ -x "$(command -v diff-so-fancy)" ]; then
-  echo "Installing diff-so-fancy..."
   brew install diff-so-fancy
+  echo "Successfully installed diff-so-fancy"
+else
+  echo "diff-so-fancy is already installed"
+fi
+
+# Create directory for Vim
+echo "Creating directory for Vim..."
+mkdir -p $HOME/.vim
+echo "Directory for Vim created"
+
+# Install Pathogen for Vim
+echo "Installing pathogen for Vim..."
+if ! [ -f "$HOME/.vim/autoload/pathogen.vim" ]; then
+  mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle" && \
+  curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
+  echo "Pathogen successfully installed"
+else
+  echo "Pathogen is alredy installed"
+fi
+
+echo "Linking color scheme"
+if ! [ -f "$HOME/.vim/colors/custom.vim" ]; then
+  mkdir -p "$HOME/.vim/colors"
+  ln vim/colors/custom.vim "$HOME/.vim/colors/custom.vim"
+  echo "Successfully linked vim colors"
+else
+  echo "Vim colors successfully linked"
 fi
 
 echo "Linkind config and startup files..."
-ln .zshrc ~/.zshrc
-ln .zprofile ~/.zprofile
-ln .vimrc ~/.vimrc
-ln .gitconfig ~/.gitconfig
-ln .gitignore_global ~/.gitignore_global
+
+echo "Linking .zshrc..."
+if ! [ -f "$HOME/.zshrc" ]; then
+  ln .zshrc "$HOME/.zshrc"
+fi
+
+echo "Linking .zprofile..."
+if ! [ -f "$HOME/.zprofile" ]; then
+  ln .zprofile "$HOME/.zprofile"
+fi
+
+echo "Linking .vimrc..."
+if ! [ -f "$HOME/.vimrc" ]; then
+  ln .vimrc "$HOME/.vimrc"
+fi
+
+echo "Linking .gitconfig..."
+if ! [ -f "$HOME/.gitconfig" ]; then
+  ln .gitconfig "$HOME/.gitconfig"
+fi
+
+echo "Linking .gitignore_global..."
+if ! [ -f "$HOME/.gitignore_global" ]; then
+  ln .gitignore_global "$HOME/.gitignore_global"
+fi
+
+echo "Successfully linked startup files"
 
 echo "Done!"
