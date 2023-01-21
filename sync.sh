0674534021d07549a335bf73b4224b7b251e1696
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-01-21 23:07:32 +0200

    add giconfig sync

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
-rm "$HOME/.vimrc" && ln ".vimrc" "$HOME/.vimrc"
-rm "$HOME/.vim/colors/custom.vim" && ln "vim/colors/custom.vim" "$HOME/.vim/colors/custom.vim"
-rm "$HOME/.gitignore_global" && ln ".gitignore_global" "$HOME/.gitignore_global"
+rm -f "$HOME/.vimrc" && ln ".vimrc" "$HOME/.vimrc"
+rm -f "$HOME/.vim/colors/custom.vim" && ln "vim/colors/custom.vim" "$HOME/.vim/colors/custom.vim"
+rm -f "$HOME/.gitignore_global" && ln ".gitignore_global" "$HOME/.gitignore_global"
+rm -f "$HOME/.gitconfig" && ln ".gitconfig" "$HOME/.gitconfig"
 echo "done"
