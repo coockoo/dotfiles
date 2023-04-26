38c699c4b71d94671ccaf1893bb101b0a76a6069
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-04-26 22:55:59 +0300

    nvim: add snippets syncing

--- sync/sync-nvim.sh
+++ sync/sync-nvim.sh
@@ -1,3 +1,4 @@
 rm -rf ~/.config/nvim/after && cp -r nvim/after ~/.config/nvim/after
 rm -rf ~/.config/nvim/lua && cp -r nvim/lua ~/.config/nvim/lua
 rm -rf ~/.config/nvim/init.lua && cp nvim/init.lua ~/.config/nvim/init.lua
+rm -rf ~/.config/nvim/snippets && cp -r nvim/snippets ~/.config/nvim/snippets
