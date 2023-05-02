6fdc24c6b44ec57e6337c755a41e22a99990cf90
Oleksandr Bondarchuk <oleksandrb@wix.com>
2023-05-02 14:28:07 +0300

    nvim: update some colors; add some snippets

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,1 +1,7 @@
 vim.cmd.colorscheme('OceanicNext')
+
+vim.api.nvim_set_hl(0, '@tag', { link = '@function' })
+vim.api.nvim_set_hl(0, '@constructor.tsx', { link = '@function' })
+vim.api.nvim_set_hl(0, '@tag.delimiter.tsx', { link = '@function' })
+vim.api.nvim_set_hl(0, '@property', { link = 'Identifier' })
+vim.api.nvim_set_hl(0, '@tag.attribute.tsx', { link = 'Identifier' })
