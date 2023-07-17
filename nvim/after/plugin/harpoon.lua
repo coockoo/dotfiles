a16ff2821bccd1a575531327d46c84218674df6f
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2023-07-17 10:35:58 +0300

    nvim: harpoon: add tabline

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,5 +1,12 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+local harpoon = require('harpoon')
+
+harpoon.setup({
+  global_settings = {
+    tabline = true
+  }
+})
 
 vim.keymap.set('n', '<leader>a', mark.add_file)
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
@@ -8,3 +15,8 @@ vim.keymap.set('n', '<C-j>', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<C-k>', function() ui.nav_file(2) end)
 vim.keymap.set('n', '<C-l>', function() ui.nav_file(3) end)
 vim.keymap.set('n', '<C-;>', function() ui.nav_file(4) end)
+
+vim.api.nvim_set_hl(0, 'HarpoonInactive', { link = 'Tabline' })
+vim.api.nvim_set_hl(0, 'HarpoonActive', { link = 'TablineSel' })
+vim.api.nvim_set_hl(0, 'HarpoonNumberActive', { link = 'TablineSel' })
+vim.api.nvim_set_hl(0, 'HarpoonNumberInactive', { link = 'Tabline' })
