b71c9603f23bf6b3adc3f87fc054cddaf49ea610
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-02-10 13:02:58 +0200

    nvim: add copilot, do some telescope rebindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,9 @@
 local builtin = require('telescope.builtin')
 local actions = require("telescope.actions")
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-o>', builtin.git_files, {})
-vim.keymap.set('n', '<C-i>', builtin.buffers, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Open telescope all files' })
+vim.keymap.set('n', '<C-o>', builtin.git_files, { desc = 'Open telescope git files' })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'Open telescope buffers' })
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
@@ -12,7 +12,7 @@ require('telescope').setup {
   defaults = {
     mappings = {
       i = {
-        ["<esc>"] = actions.close,
+        ["<Esc>"] = actions.close,
       },
     },
     layout_config = {
--- nvim/lua/coockoo/packer.lua
+++ nvim/lua/coockoo/packer.lua
@@ -69,4 +69,6 @@ return require('packer').startup(function(use)
     'airblade/vim-gitgutter',
     branch = 'main'
   }
+
+  use 'github/copilot.vim'
 end)
