18a3ceed47eea79727b3a21439aca312275beaeb
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-02-13 11:50:21 +0200

    nvim: colors: adjust a bit

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -11,8 +11,9 @@ for i = 1, 6, 1 do
   vim.api.nvim_set_hl(0, '@markup.heading.' .. i .. '.markdown', { link = 'Title' })
   vim.api.nvim_set_hl(0, '@markup.heading.' .. i .. '.marker.markdown', { link = 'Title' })
 end
-vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { link = 'Comment' })
+vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { link = 'Float' })
+vim.api.nvim_set_hl(0, '@markup.raw.block.markdown', { link = 'Comment' })
 vim.api.nvim_set_hl(0, '@markup.link.url.markdown', { link = 'Underlined' })
-vim.api.nvim_set_hl(0, '@markup.link.markdown_inline', { link = 'Float' })
-vim.api.nvim_set_hl(0, '@markup.link.label.markdown_inline', { link = 'Float' })
-vim.api.nvim_set_hl(0, '@markup.link.label.markdown', { link = 'Float' })
+vim.api.nvim_set_hl(0, '@markup.link.markdown_inline', { link = 'Underlined' })
+vim.api.nvim_set_hl(0, '@markup.link.label.markdown_inline', { link = 'Underlined' })
+vim.api.nvim_set_hl(0, '@markup.link.label.markdown', { link = 'Underlined' })
