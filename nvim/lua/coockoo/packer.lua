479bab7b7893f478e43edaf45c17f5101af0e0ba
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-09-13 18:06:51 +0300

    nvim: bring back null-ls (for now)

--- nvim/lua/coockoo/packer.lua
+++ nvim/lua/coockoo/packer.lua
@@ -17,6 +17,9 @@ return require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
 
+  use 'neovim/nvim-lspconfig'
+  use 'jose-elias-alvarez/null-ls.nvim'
+
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
