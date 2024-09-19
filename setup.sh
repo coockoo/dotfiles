70657e952c660ce255f2451d3f28cb9308f147cc
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-09-19 11:35:14 +0300

    setup: improve even more

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-# installa homebrew
+# install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
+chmod -R go-w "$(brew --prefix)/share"
 # let homebrew install some nice deps
-brew install z fzf fnm diff-so-fancy the_silver_searcher nvim
+brew install z fzf fnm diff-so-fancy the_silver_searcher nvim rg
 brew install --cask 1password iterm2 spotify firefox
 # install packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
