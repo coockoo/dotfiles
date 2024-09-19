b9575eb4e3811bae301ebd23623b76ec02183358
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-09-19 10:57:04 +0300

    setup: install packer

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # let homebrew install some nice deps
 brew install z fzf fnm diff-so-fancy the_silver_searcher nvim
 brew install --cask 1password iterm2 spotify firefox
+# install packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
