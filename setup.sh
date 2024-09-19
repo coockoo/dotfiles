fb70b44321002f21a3dfbfa5a4f62f8c1b03a2fb
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-09-19 12:35:19 +0300

    setup: improve it event more

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ chmod -R go-w "$(brew --prefix)/share"
 # let homebrew install some nice deps
 brew install z fzf fnm diff-so-fancy the_silver_searcher nvim rg
 brew install --cask 1password iterm2 spotify firefox
+# some of the packer deps need npm and node
+fnm install --lts
 # install packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
