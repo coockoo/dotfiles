99b6f411bc1cba403760b9fd262e54157380b82b
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-10-18 13:07:31 +0300

    nvim: telescope: adjust some kemap definitions

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local builtin = require('telescope.builtin')
 local actions = require("telescope.actions")
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Open telescope all files' })
-vim.keymap.set('n', '<C-o>', builtin.git_files, { desc = 'Open telescope git files' })
-vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'Open telescope buffers' })
-vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'Open telescope live grep' })
-vim.keymap.set('n', '<leader>r', builtin.resume, { desc = 'Resume telescope' })
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'telescope: [p]roject files' })
+vim.keymap.set('n', '<C-o>', builtin.git_files, { desc = 'telescope: [o]pen git files' })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'telescope: [b]uffers' })
+vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'telescope: live [g]rep' })
+vim.keymap.set('n', '<leader>r', builtin.resume, { desc = 'telescope: [r]esume' })
 
 require('telescope').setup {
   defaults = {
