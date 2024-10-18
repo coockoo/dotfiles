26f2d93e237903ca4a29fbd498668c2638acb1a2
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-10-18 21:08:33 +0300

    nvim: lsp: add markdown and css lsps

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -96,3 +96,11 @@ lspconfig.efm.setup({
 lspconfig.hls.setup({
   filetypes = { 'haskell', 'cabal' },
 })
+
+lspconfig.marksman.setup({})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+lspconfig.cssls.setup({
+  capabilities = capabilities,
+})
