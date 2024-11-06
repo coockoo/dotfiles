13a3ef49cf2a6c354cda01b7d13a884811c0da11
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-11-06 18:46:01 +0200

    nvim: treesitter: cleanup config

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,13 +1,15 @@
-require 'nvim-treesitter.configs'.setup({
+local success, configs = pcall(require, 'nvim-treesitter.configs')
+if not success then
+  vim.notify('treesitter: cannot require nvim-treesitter.configs', vim.log.levels.ERROR)
+  return
+end
+
+configs.setup({
+  modules = {},
   ensure_installed = { 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'typescript', 'html', 'markdown', 'markdown_inline' },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
+  ignore_install = {},
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
   highlight = {
     enable = true,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -15,8 +17,5 @@ require 'nvim-treesitter.configs'.setup({
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
-  },
-  autotag = {
-    enable = true,
   },
 })
