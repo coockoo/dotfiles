c0a01ce99a6d0a7f7ab154b2a72f075a6716b4b8
Oleksandr Bondarchuk <oleksandrb@wix.com>
2024-12-29 17:11:10 +0200

    some colors changes

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,20 +1,25 @@
 vim.cmd.colorscheme('OceanicNext')
 
-vim.api.nvim_set_hl(0, '@tag', { link = '@function' })
-vim.api.nvim_set_hl(0, '@constructor.tsx', { link = '@function' })
-vim.api.nvim_set_hl(0, '@tag.builtin.tsx', { link = '@function' })
-vim.api.nvim_set_hl(0, '@tag.delimiter.tsx', { link = '@function' })
-vim.api.nvim_set_hl(0, '@tag.attribute.tsx', { link = 'Identifier' })
-vim.api.nvim_set_hl(0, 'diffAdded', { link = 'DiffNewFile' })
-vim.api.nvim_set_hl(0, 'diffRemoved', { link = 'DiffFile' })
-
-for i = 1, 6, 1 do
-  vim.api.nvim_set_hl(0, '@markup.heading.' .. i .. '.markdown', { link = 'Title' })
-  vim.api.nvim_set_hl(0, '@markup.heading.' .. i .. '.marker.markdown', { link = 'Title' })
+---@param name string
+---@param val vim.api.keyset.highlight
+local function hl(name, val)
+  vim.api.nvim_set_hl(0, name, val)
 end
-vim.api.nvim_set_hl(0, '@markup.raw.markdown_inline', { link = 'Float' })
-vim.api.nvim_set_hl(0, '@markup.raw.block.markdown', { link = 'Comment' })
-vim.api.nvim_set_hl(0, '@markup.link.url.markdown', { link = 'Underlined' })
-vim.api.nvim_set_hl(0, '@markup.link.markdown_inline', { link = 'Underlined' })
-vim.api.nvim_set_hl(0, '@markup.link.label.markdown_inline', { link = 'Underlined' })
-vim.api.nvim_set_hl(0, '@markup.link.label.markdown', { link = 'Underlined' })
+
+hl('@tag', { link = '@function' })
+hl('@constructor.tsx', { link = '@function' })
+hl('@tag.builtin.tsx', { link = '@function' })
+hl('@tag.delimiter.tsx', { link = '@function' })
+hl('@tag.attribute.tsx', { link = 'Identifier' })
+hl('diffAdded', { link = 'DiffNewFile' })
+hl('diffRemoved', { link = 'DiffFile' })
+for i = 1, 6, 1 do
+  hl('@markup.heading.' .. i .. '.markdown', { link = 'Title' })
+  hl('@markup.heading.' .. i .. '.marker.markdown', { link = 'Title' })
+end
+hl('@markup.raw.markdown_inline', { link = 'Float' })
+hl('@markup.raw.block.markdown', { link = 'Comment' })
+hl('@markup.link.url.markdown', { link = 'Underlined' })
+hl('@markup.link.markdown_inline', { link = 'Underlined' })
+hl('@markup.link.label.markdown_inline', { link = 'Underlined' })
+hl('@markup.link.label.markdown', { link = 'Underlined' })
