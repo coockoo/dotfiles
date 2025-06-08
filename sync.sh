b7df9169630aff554904e39a81454cee0b870dd0
Oleksandr Bondarchuk <oleksandrb@wix.com>
2025-06-08 10:05:12 +0300

    let's try this skhd thing

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 mkdir -p $HOME/.config/ghostty
 rm -f $HOME/.config/ghostty/config && ln ghostty/config $HOME/.config/ghostty/config
+mkdir -p $HOME/.config/skhd
+rm -f $HOME/.config/skhd/skhdrc && ln skhdrc $HOME/.config/skhd/skhdrc
 rm -f "$HOME/.zshrc" && ln ".zshrc" "$HOME/.zshrc"
 rm -f "$HOME/.gitignore_global" && ln ".gitignore_global" "$HOME/.gitignore_global"
 rm -f "$HOME/.gitconfig" && ln ".gitconfig" "$HOME/.gitconfig"
