de69723ce67c321ab132c880be4522ca856b3dba
Oleksandr Bondarchuk <oleksandrb@wix.com>
2025-08-11 17:47:48 +0300

    bin: window_manager: configurable percentages

--- bin/window_manager.swift
+++ bin/window_manager.swift
@@ -4,14 +4,20 @@ import ApplicationServices
 import Cocoa
 
 let args = CommandLine.arguments
+let pString = ProcessInfo.processInfo.environment["WM_SPLIT_PERCENT"] ?? "0.8"
+let percentage = Double(pString) ?? 0.8
 
-// TODO: make percentages configurable
+// to change env variable - edit
+// $HOME/Library/LaunchAgents/com.koekeishiya.skhd.plist
+// and add env variable there
+// TODO: how to make it more usable?
+
 if args.count < 2 {
   print("Usage: \(args[0]) <action>")
   print("Actions:")
   print("  full-screen: set window to full screen")
-  print("  split-west: set window to west split (80%)")
-  print("  split-east: set window to east split (80%)")
+  print("  split-west: set window to west split (\(percentage))")
+  print("  split-east: set window to east split (\(percentage))")
   exit(1)
 }
 
@@ -35,10 +41,10 @@ func getSizeAndPosition(action: String) -> (CGSize, CGPoint) {
 
   var position: CGPoint?
   if action == "split-west" {
-    width = screenFrame.width * 0.8
+    width = screenFrame.width * percentage
     position = CGPoint(x: 0, y: 0)
   } else if action == "split-east" {
-    width = screenFrame.width * 0.8
+    width = screenFrame.width * percentage
     position = CGPoint(x: screenFrame.width - width, y: 0)
   } else if action == "full-screen" {
     position = CGPoint(x: 0, y: 0)
